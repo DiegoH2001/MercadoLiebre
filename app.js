9288e302ae5209777db69a6634155e4b0574b79c
DiegoH2001 <diego20hidalgo20@gmail.com>
2023-10-16 21:40:49 -0300

    maquetado mercado liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app=express()
+const PORT = 3000;
+
+const publicPath= path.join(__dirname, './public')
+app.use(express.static(publicPath))
+
+app.listen(PORT,() =>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+})
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
